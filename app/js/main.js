51cd44c378eefd9da157373da50c02221c75156b
ekaterynakravchuk <ekateryna.kravchuk@gmail.com>
2022-05-19 17:27:09 +0300

    blog

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function() {
     $('.reviews__slider').slick();
+    $('.blog__slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<button class="blog__prev-arrow"><img src="images/prev-arrow.svg"></button>',
+        nextArrow: '<button class="blog__next-arrow"><img src="images/next-arrow.svg"></button>'
+    });
 })
